cda44880a96a963f480d98f58bb65b8327a992fc
James McNeil <james.r.mcneil@gmail.com>
2014-10-13 16:30:24 +0100

    Set up blank ember.js app on top of Ruby on Rails

---
+++ app/assets/javascripts/router.js.coffee
@@ -0,0 +1,5 @@
+# For more information see: http://emberjs.com/guides/routing/
+
+Godfather.Router.map ()->
+  # @resource('posts')
+
---
+++ config/initializers/active_model_serializers.rb
@@ -0,0 +1,10 @@
+#  The following code has been taken from an angular tutorial. Use if JSON objects don't render properly
+# Source: https://shellycloud.com/blog/2013/10/how-to-integrate-angularjs-with-rails-4
+
+# ActiveSupport.on_load(:active_model_serializers) do
+#   # Disable for all serializers (except ArraySerializer)
+#   ActiveModel::Serializer.root = false
+
+#   # Disable for ArraySerializer
+#   ActiveModel::ArraySerializer.root = false
+# end
